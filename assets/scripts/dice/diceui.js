7638ee157d044317bb35825e4584cc2d079a72cb
lmplaczkiewicz <Frederick.Placzkiewicz@LibertyMutual.com>
2020-02-12 20:59:38 -0500

    images are shown now

--- assets/scripts/dice/diceui.js
+++ assets/scripts/dice/diceui.js
@@ -5,53 +5,54 @@ const showDiceSuccess = function (ringDiceResults, skillDiceResults) {
   const ringDiceTiles = []
   const skillDiceTiles = []
   for (let i = 0; i < ringDiceResults.length; i++) {
+    console.log('In ring dice')
     if (ringDiceResults[i] === 1) {
-      ringDiceTiles.push('../images/black.png')
+      ringDiceTiles.push('./assets/images/black.png')
     } else if (ringDiceResults[i] === 2) {
-      ringDiceTiles.push('../images/blacket.png')
+      ringDiceTiles.push('./assets/images/blacket.png')
     } else if (ringDiceResults[i] === 3) {
-      ringDiceTiles.push('../images/blacko.png')
+      ringDiceTiles.push('./assets/images/blacko.png')
     } else if (ringDiceResults[i] === 4) {
-      ringDiceTiles.push('../images/blackot.png')
+      ringDiceTiles.push('./assets/images/blackot.png')
     } else if (ringDiceResults[i] === 5) {
-      ringDiceTiles.push('../images/blackst.png')
+      ringDiceTiles.push('./assets/images/blackst.png')
     } else if (ringDiceResults[i] === 6) {
-      ringDiceTiles.push('../images/blacks.png')
+      ringDiceTiles.push('./assets/images/blacks.png')
     }
   }
   console.log(ringDiceTiles)
   for (let i = 0; i < skillDiceResults.length; i++) {
     if (skillDiceResults[i] === 1) {
-      skillDiceTiles.push('../images/white.png')
+      skillDiceTiles.push('./assets/images/white.png')
     } else if (skillDiceResults[i] === 2) {
-      skillDiceTiles.push('../images/white.png')
+      skillDiceTiles.push('./assets/images/white.png')
     } else if (skillDiceResults[i] === 3) {
-      skillDiceTiles.push('../images/whiteo.png')
+      skillDiceTiles.push('./assets/images/whiteo.png')
     } else if (skillDiceResults[i] === 4) {
-      skillDiceTiles.push('../images/whiteo.png')
+      skillDiceTiles.push('./assets/images/whiteo.png')
     } else if (skillDiceResults[i] === 5) {
-      skillDiceTiles.push('../images/whiteo.png')
+      skillDiceTiles.push('./assets/images/whiteo.png')
     } else if (skillDiceResults[i] === 6) {
-      skillDiceTiles.push('../images/whitest.png')
+      skillDiceTiles.push('./assets/images/whitest.png')
     } else if (skillDiceResults[i] === 7) {
-      skillDiceTiles.push('../images/whitest.png')
+      skillDiceTiles.push('./assets/images/whitest.png')
     } else if (skillDiceResults[i] === 8) {
-      skillDiceTiles.push('../images/whites.png')
+      skillDiceTiles.push('./assets/images/whites.png')
     } else if (skillDiceResults[i] === 9) {
-      skillDiceTiles.push('../images/whites.png')
+      skillDiceTiles.push('./assets/images/whites.png')
     } else if (skillDiceResults[i] === 10) {
-      skillDiceTiles.push('../images/whiteso.png')
+      skillDiceTiles.push('./assets/images/whiteso.png')
     } else if (skillDiceResults[i] === 11) {
-      skillDiceTiles.push('../images/whiteet.png')
+      skillDiceTiles.push('./assets/images/whiteet.png')
     } else if (skillDiceResults[i] === 12) {
-      skillDiceTiles.push('../images/whitee.png')
+      skillDiceTiles.push('./assets/images/whitee.png')
     }
   }
   console.log(skillDiceTiles)
   const showRingDiceHtml = showDiceTemplate({dice: ringDiceTiles})// quests: data.quests })
-  $('#diceTileDisplay').html(showRingDiceHtml)
+  $('#diceTileDisplay').append(showRingDiceHtml)
   const showSkillDiceHtml = showDiceTemplate({dice: skillDiceTiles})// quests: data.quests })
-  $('#diceTileDisplay').html(showSkillDiceHtml)
+  $('#diceTileDisplay').append(showSkillDiceHtml)
 }
 
 module.exports = {
