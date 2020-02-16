c47987dfd0d22431b88a67c87e31b174b1185d6d
lmplaczkiewicz <Frederick.Placzkiewicz@LibertyMutual.com>
2020-02-15 22:03:41 -0500

    add url for images

--- assets/scripts/dice/diceui.js
+++ assets/scripts/dice/diceui.js
@@ -1,7 +1,7 @@
 // const store = require('../store')
 const showDiceTemplate = require('../templates/diceTiles.handlebars')
 const showSkillDiceTemplate = require('../templates/skillDiceTiles.handlebars')
-const moment = require('moment');
+const moment = require('moment')
 let ringDiceNumberToRoll
 let skillDiceNumberToRoll
 let ringDiceTiles = []
@@ -18,64 +18,64 @@ const showDiceSuccess = function (ringDiceResults, skillDiceResults) {
   for (let i = 0; i < ringDiceResults.length; i++) {
     console.log('In ring dice')
     if (ringDiceResults[i] === 1) {
-      ringDiceTiles.push('./assets/images/black.png')
+      ringDiceTiles.push('https://raw.githubusercontent.com/lmplaczkiewicz/Front-TTT/master/assets/images/black.png')
     } else if (ringDiceResults[i] === 2) {
-      ringDiceTiles.push('./assets/images/blacket.png')
+      ringDiceTiles.push('https://raw.githubusercontent.com/lmplaczkiewicz/Front-TTT/master/assets/images/blacket.png')
       ringDiceExplosionCount++
     } else if (ringDiceResults[i] === 3) {
-      ringDiceTiles.push('./assets/images/blacko.png')
+      ringDiceTiles.push('https://raw.githubusercontent.com/lmplaczkiewicz/Front-TTT/master/assets/images/blacko.png')
     } else if (ringDiceResults[i] === 4) {
-      ringDiceTiles.push('./assets/images/blackot.png')
+      ringDiceTiles.push('https://raw.githubusercontent.com/lmplaczkiewicz/Front-TTT/master/assets/images/blackot.png')
     } else if (ringDiceResults[i] === 5) {
-      ringDiceTiles.push('./assets/images/blackst.png')
+      ringDiceTiles.push('https://raw.githubusercontent.com/lmplaczkiewicz/Front-TTT/master/assets/images/blackst.png')
     } else if (ringDiceResults[i] === 6) {
-      ringDiceTiles.push('./assets/images/blacks.png')
+      ringDiceTiles.push('https://raw.githubusercontent.com/lmplaczkiewicz/Front-TTT/master/assets/images/blacks.png')
     }
   }
   console.log(ringDiceTiles)
   for (let i = 0; i < skillDiceResults.length; i++) {
     if (skillDiceResults[i] === 1 || skillDiceResults[i] === 2) {
-      skillDiceTiles.push('./assets/images/white.png')
+      skillDiceTiles.push('https://raw.githubusercontent.com/lmplaczkiewicz/Front-TTT/master/assets/images/white.png')
     } else if (skillDiceResults[i] === 3 || skillDiceResults[i] === 4 || skillDiceResults[i] === 5) {
-      skillDiceTiles.push('./assets/images/whiteo.png')
+      skillDiceTiles.push('https://raw.githubusercontent.com/lmplaczkiewicz/Front-TTT/master/assets/images/whiteo.png')
     } else if (skillDiceResults[i] === 6 || skillDiceResults[i] === 7) {
-      skillDiceTiles.push('./assets/images/whitest.png')
+      skillDiceTiles.push('https://raw.githubusercontent.com/lmplaczkiewicz/Front-TTT/master/assets/images/whitest.png')
     } else if (skillDiceResults[i] === 8 || skillDiceResults[i] === 9) {
-      skillDiceTiles.push('./assets/images/whites.png')
+      skillDiceTiles.push('https://raw.githubusercontent.com/lmplaczkiewicz/Front-TTT/master/assets/images/whites.png')
     } else if (skillDiceResults[i] === 10) {
-      skillDiceTiles.push('./assets/images/whiteso.png')
+      skillDiceTiles.push('https://raw.githubusercontent.com/lmplaczkiewicz/Front-TTT/master/assets/images/whiteso.png')
     } else if (skillDiceResults[i] === 11) {
-      skillDiceTiles.push('./assets/images/whiteet.png')
+      skillDiceTiles.push('https://raw.githubusercontent.com/lmplaczkiewicz/Front-TTT/master/assets/images/whiteet.png')
       skillDiceExplosionCount++
     } else if (skillDiceResults[i] === 12) {
-      skillDiceTiles.push('./assets/images/whitee.png')
+      skillDiceTiles.push('https://raw.githubusercontent.com/lmplaczkiewicz/Front-TTT/master/assets/images/whitee.png')
       skillDiceExplosionCount++
     }
   }
   console.log(skillDiceTiles)
-  const time = moment().format('LTS');
+  const time = moment().format('LTS')
   const showRingDiceHtml = showDiceTemplate({dice: ringDiceTiles})
-  if(ringDiceTiles.length > 0){
+  if (ringDiceTiles.length > 0) {
     $('#ringDiceTileDisplay').prepend(showRingDiceHtml)
     $('#diceTime').text(time)
-    if(ringDiceExplosionCount > 0){
-      console.log('explosion rdice exist');
+    if (ringDiceExplosionCount > 0) {
+      console.log('explosion rdice exist')
       showRDiceExplosionElement()
     }
   }
   const showSkillDiceHtml = showSkillDiceTemplate({dice: skillDiceTiles})
-  if(skillDiceTiles.length > 0){
+  if (skillDiceTiles.length > 0) {
     $('#skillDiceTileDisplay').prepend(showSkillDiceHtml)
     $('#diceTimeSkill').text(time)
   }
 }
 
 const showRDiceExplosionElement = function () {
-    // rDiceExplosionElement.style.display = 'block'
+  // rDiceExplosionElement.style.display = 'block'
 }
 
 const hideRDiceExplosionElement = function () {
-    rDiceExplosionElement.style.display = 'none'
+  rDiceExplosionElement.style.display = 'none'
 }
 
 module.exports = {
